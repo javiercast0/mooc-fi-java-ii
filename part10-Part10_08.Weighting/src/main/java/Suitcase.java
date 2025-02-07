f09c49e278c8f788f0a31e24434e8209bee30782
root <root@ASUS-JAVI.>
2025-02-07 19:22:48 +0100

    10-08 cargo

--- part10-Part10_08.Weighting/src/main/java/Suitcase.java
+++ part10-Part10_08.Weighting/src/main/java/Suitcase.java
@@ -20,12 +20,7 @@ public class Suitcase {
     }
 
     public int totalWeight() {
-        int summa = 0;
-        int indeksi = 0;
-        while (indeksi < this.Items.size()) {
-        summa += this.Items.get(indeksi).getWeight();
-        indeksi++;
-        }
+        int summa = Items.stream().mapToInt(Item::getWeight).sum();
         return summa;
     }
 
