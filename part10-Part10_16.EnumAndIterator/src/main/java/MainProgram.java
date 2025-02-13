2dd75dcd8bcc532836fc57cf76af36d8ea85c5e0
Javier Castillo <javiercast00112@gmail.com>
2025-02-13 17:14:44 +0100

    Did Part 11 exercises

--- part10-Part10_16.EnumAndIterator/src/main/java/MainProgram.java
+++ part10-Part10_16.EnumAndIterator/src/main/java/MainProgram.java
@@ -4,4 +4,5 @@ public class MainProgram {
     public static void main(String[] args) {
         // test your classes here
     }
+    
 }
---
+++ part11-Part11_07.BiggerClassDiagram/src/main/java/D.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author qeiry
+ */
+public class D implements IA{
+    
+    private IA ia;
+    public void metodoIA(){
+        
+    }
+}
