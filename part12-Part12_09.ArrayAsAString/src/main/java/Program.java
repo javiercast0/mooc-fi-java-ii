82b7244bdb608b2b85e71adc35e2d111c0063e7b
Javier Castillo <javiercast00112@gmail.com>
2025-02-28 19:41:08 +0100

    13-01 to 13-04

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -4,5 +4,16 @@ public class Program {
     public static void main(String[] args) {
         // Test your method here
     }
+    
+    public static String arrayAsString(int[][] array){
+        StringBuilder sb = new StringBuilder();
+        for (int row = 0 ; row < array.length ; row++){
+            for (int column = 0; column < array[row].length ; column++){
+                sb.append(array[row][column]);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 
 }
